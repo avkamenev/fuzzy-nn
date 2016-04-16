4d8b939805faf133763c1ec38ad60554ab23e60c
andre-vk <andre_mp@mail.ru>
2016-04-16 15:08:09 +0300

    init

---
+++ read_data.py
@@ -0,0 +1,30 @@
+import pandas as pd
+import numpy as np
+from sklearn.ensemble import RandomForestRegressor
+
+data = pd.read_csv('train1.csv', delimiter=';')
+
+#times = [row.split()[1].split(':') for row in data.iloc[:,0]]
+#times = [int(row[0])*60 + int(row[1]) for row in times]
+#data.iloc[:,0] = times
+
+x = data.iloc[:,1:5].values
+y = data.iloc[:,5].values
+
+
+rfc = RandomForestRegressor(n_estimators=100)
+rfc = rfc.fit(x, y)
+''+str(round(sum(abs(rfc.predict(x) - y)/abs(y))/len(y)*100,2))+'%'
+
+
+test_data = pd.read_csv('test1.csv', delimiter=';')
+x_test = test_data.iloc[:,1:5].values
+y_test = test_data.iloc[:,5].values
+''+str(round(sum(abs(rfc.predict(x_test) - y_test)/abs(y_test))/len(y_test)*100,2))+'%'
+
+
+
+
+
+
+
