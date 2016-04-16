37182ac0a2c46558f72ee73495c11e0b4e52dbd9
andre-vk <andre_mp@mail.ru>
2016-04-16 20:41:45 +0300

    init

---
+++ generate_y_model.py
@@ -0,0 +1,29 @@
+import scipy
+
+number_of_rules = 2
+number_of_mfs = 2
+number_of_vars = x.shape[1]
+
+mfs = [[0,1],[1,1]]
+scipy.stats.norm(1, 1)
+
+test = x[1]
+
+w_value = 0
+w_value = w_value + scipy.stats.norm(mfs[0][0], mfs[0][1]).pdf(test[0])
+w_value = w_value + scipy.stats.norm(mfs[1][0], mfs[1][1]).pdf(test[1])
+
+for i1 in range()
+
+
+beta = w_value/sum(w_value)
+
+from itertools import product
+list(product([0, 1], [0, 1], [0, 1], [0, 1]))
+[",".join(map(str,comb)) for comb in product([0, 1], [0, 1], [0, 1], [0, 1])]
+
+
+
+
+
+
