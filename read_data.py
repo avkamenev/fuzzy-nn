af46b1752835af0d7b46c68644605b21a513a47b
Andrey Kamenev <andre_mp@mail.ru>
2016-04-19 21:43:29 +0300

    y_model

--- read_data.py
+++ read_data.py
@@ -2,30 +2,46 @@ import pandas as pd
 import numpy as np
 from sklearn.ensemble import RandomForestRegressor
 from sklearn import preprocessing
+CREATE_TEST_SPLIT = False
 
-data = pd.read_csv('train1.csv', delimiter=';')
+
+if CREATE_TEST_SPLIT:
+    data = pd.read_csv('data/data_3k.csv', delimiter=',', index_col = False)
+    data = data.drop(np.where(data['y']==0)[0])
+    indices = range(len(data))
+    np.random.shuffle(indices)
+    sep_index = int(0.8*len(data))
+    training, test = indices[:sep_index], indices[sep_index:]
+    train_data = data.iloc[training]
+    test_data = data.iloc[test]
+    train_data.to_csv('data/train.csv', index=False)
+    test_data.to_csv('data/test.csv', index=False)
+
 
 #times = [row.split()[1].split(':') for row in data.iloc[:,0]]
 #times = [int(row[0])*60 + int(row[1]) for row in times]
 #data.iloc[:,0] = times
 
-x = data.iloc[:,1:5].values
+train_data = pd.read_csv('data/train.csv', delimiter=',', index_col = False)
+x = train_data.iloc[:,:4].values
+test_data = pd.read_csv('data/test.csv', delimiter=',', index_col = False)
+x_test = test_data.iloc[:,:4].values
+
 min_max_scaler = preprocessing.MinMaxScaler()
-x = min_max_scaler.fit_transform(x)
-y = data.iloc[:,5].values
+min_max_scaler.fit(np.row_stack((x,x_test)))
+
+x = min_max_scaler.transform(x)
+x_test = min_max_scaler.transform(x_test)
+
+y = train_data.iloc[:,4].values
+y_test = test_data.iloc[:,4].values
 
 
-rfc = RandomForestRegressor(n_estimators=1000)
+rfc = RandomForestRegressor(n_estimators=100)
 rfc = rfc.fit(x, y)
-''+str(round(sum(abs(rfc.predict(x) - y)/abs(y))/len(y)*100,2))+'%'
+round(sum(abs(rfc.predict(x) - y)/abs(y))/len(y),4)
 
-
-test_data = pd.read_csv('test1.csv', delimiter=';')
-x_test = test_data.iloc[:,1:5].values
-min_max_scaler = preprocessing.MinMaxScaler()
-x_test = min_max_scaler.fit_transform(x_test)
-y_test = test_data.iloc[:,5].values
-''+str(round(sum(abs(rfc.predict(x_test) - y_test)/abs(y_test))/len(y_test)*100,2))+'%'
+round(sum(abs(rfc.predict(x_test) - y_test)/abs(y_test))/len(y_test),4)
 
 
 
